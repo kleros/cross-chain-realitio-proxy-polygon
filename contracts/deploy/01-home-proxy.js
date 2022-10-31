874b76d7102fb3966ca671a7eaa7db74d91244ae
unknownunknown1 <time_arrow@bk.ru>
2022-10-31 15:43:52 +0400

    chore: update deployment scripts

--- contracts/deploy/01-home-proxy.js
+++ contracts/deploy/01-home-proxy.js
@@ -1,18 +1,18 @@
-const HOME_CHAIN_IDS = [80001];
+const HOME_CHAIN_IDS = [80001, 137];
 const paramsByChainId = {
   80001: {
     fxChild: "0xCf73231F28B7331BBe3124B907840A94851f9f11",
     realitio: "0x92115220c28e78312cce86f3d1de652cfbd0357a",
     foreignChainId: 5,
     metadata:
-      '{"tos":"ipfs://QmaUr6hnSVxYD899xdcn2GUVtXVjXoSXKZbce3zFtGWw4H/Question_Resolution_Policy.pdf", "foreignProxy":true}',
+      '{"tos":"ipfs://QmNV5NWwCudYKfiHuhdWxccrPyxs4DnbLGQace2oMKHkZv/Question_Resolution_Policy.pdf", "foreignProxy":true}',
   },
   137: {
     fxChild: "0x8397259c983751DAf40400790063935a11afa28a",
     realitio: "0x60573B8DcE539aE5bF9aD7932310668997ef0428",
     foreignChainId: 1,
     metadata:
-      '{"tos":"ipfs://QmaUr6hnSVxYD899xdcn2GUVtXVjXoSXKZbce3zFtGWw4H/Question_Resolution_Policy.pdf", "foreignProxy":true}',
+      '{"tos":"ipfs://QmNV5NWwCudYKfiHuhdWxccrPyxs4DnbLGQace2oMKHkZv/Question_Resolution_Policy.pdf", "foreignProxy":true}',
   },
 };
 
--- contracts/deploy/02-foreign-proxy.js
+++ contracts/deploy/02-foreign-proxy.js
@@ -1,4 +1,4 @@
-const FOREIGN_CHAIN_IDS = [5];
+const FOREIGN_CHAIN_IDS = [5, 1];
 const paramsByChainId = {
   5: {
     checkpointManager: "0x2890bA17EfE978480615e330ecB65333b880928e",
@@ -6,7 +6,7 @@ const paramsByChainId = {
     arbitrator: "0x1128eD55ab2d796fa92D2F8E1f336d745354a77A", // KlerosLiquid Goerli
     arbitratorExtraData:
       "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-    metaEvidence: "/ipfs/Qmcg1JYNqtuVJUbVk79DuFwWaZwzduiwndoURJa37kZZ2F/realitio.json",
+    metaEvidence: "/ipfs/QmYAomciorEQ3pisZP7DuzFN9TGhT5YaQYMJfnnw3ZJQYV/realitio.json",
     winnerMultiplier: 3000,
     loserMultiplier: 7000,
     loserAppealPeriodMultiplier: 5000,
@@ -16,8 +16,8 @@ const paramsByChainId = {
     fxRoot: "0xfe5e5D361b2ad62c541bAb87C45a0B9B018389a2",
     arbitrator: "0x988b3a538b618c7a603e1c11ab82cd16dbe28069", // KlerosLiquid address
     arbitratorExtraData:
-      "0x000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001f4",
-    metaEvidence: "/ipfs/Qmc6bWTzPMFeRx9VWHwnDpDXfimwNsvnEgJo3gymg37rRd/realitio.json",
+      "0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001f", // General Court - 31 jurors
+    metaEvidence: "/ipfs/QmXWr4ZWCpBYtAHNHzTbKW9SkV1MwQicpWfthhDHkNYxKk/realitio.json",
     winnerMultiplier: 3000,
     loserMultiplier: 7000,
     loserAppealPeriodMultiplier: 5000,
@@ -59,7 +59,7 @@ async function deployForeignProxy({ deployments, getNamedAccounts, getChainId })
 
   // Note that Home proxy should be manually linked post-deployment, using setFxChildTunnel() of foreignProxy.
 
-  console.log("Foregin Proxy:", foreignProxy.address);
+  console.log("Foreign Proxy:", foreignProxy.address);
 }
 
 deployForeignProxy.tags = ["ForeignChain"];
